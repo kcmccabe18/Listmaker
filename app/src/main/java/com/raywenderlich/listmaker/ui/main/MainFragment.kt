f89ea170fbc075dc18086503f9747b5d193dbfe1
Kerry McCabe <kmccab6@live.spcollege.edu>
2022-03-29 17:39:33 -0400

    Chap 7

--- app/src/main/java/com/raywenderlich/listmaker/ui/main/MainFragment.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/MainFragment.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raywenderlich.listmaker.R
+import com.raywenderlich.listmaker.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -20,7 +24,13 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
